22561fe6515e294384fa3290cd010a879f2b7150
arnoldout <arnoldout@gmail.com>
2016-09-25 17:17:30 +0100

    push

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/russross/blackfriday"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"Heroku-Go_FirstDeployedApp\models"
 )
-
+UserController struct {  
+    session *mgo.Session
+}
 func main() {
 	port := os.Getenv("PORT")
 
@@ -20,10 +25,28 @@ func main() {
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
+	
+	uc := controllers.NewUserController(getSession())
 
 	router.GET("/mark", func(c *gin.Context) {
+		u := models.User{
+	          Name:   "Bob Smith",
+	          Gender: "male",
+	          Age:    50,
+	          Id:     p.ByName("id"),
+        }
 		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
 	})
 
 	router.Run(":" + port)
+}
+func getSession() *mgo.Session {
+	// Connect to our local mongo
+	s, err := mgo.Dial(" mongodb://<heroku_1pkmmgpc>:<mongopassword1>@ds041556.mlab.com:41556/heroku_1pkmmgpc")
+
+	// Check if connection error, is mongo running?
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
